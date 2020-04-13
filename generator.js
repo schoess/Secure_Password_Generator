f4cbfe2b701e35f36d341990da0a6f6a831589b7
nick <schoessnick@gmail.com>
2020-04-13 17:15:23 -0500

    added generator.js to build password generator logic

---
+++ generator.js
@@ -0,0 +1,10 @@
+function generate ( length = a ){
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    var numbers = "0123456789";
+    var symbols = "!#$&%'()*+,-./:;<=>?@^_~`{|}";
+
+    var all = uppercase + lowercase + numbers + symbols;
+
+    var password = ""
+}
