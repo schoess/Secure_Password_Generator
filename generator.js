7224bd993f3a972cb25a5819100c61eacde14d0d
nick <schoessnick@gmail.com>
2020-04-15 13:54:08 -0500

    updated outline.txt

--- generator.js
+++ generator.js
@@ -1,12 +1,17 @@
 function generate ( length = a ){
-    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowercase = "abcdefghijklmnopqrstuvwxyz";
-    var numbers = "0123456789";
-    var symbols = "!#$&%'()*+,-./:;<=>?@^_~`{|}";
+    var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var symbols = ["!", "#", "$", "&", "%", "'", "(", ")", "*", "+", "-", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "~", "`", "{", "|", "}", "."];
 
     var all = uppercase + lowercase + numbers + symbols;
 
     var password = ""
+
+
+
+
+
 
     for (var index = 0; index < length; index++) {
         var character = Math.floor(Math.random() * all.length);
