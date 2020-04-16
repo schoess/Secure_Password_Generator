2bf2735d4abbdfc7f298be53ad70a621f05bf753
nick <schoessnick@gmail.com>
2020-04-16 10:33:43 -0500

    created logic and confirms of password generator. linked them in their respective elements of the index.html page

--- generator.js
+++ generator.js
@@ -1,22 +1,35 @@
-function generate ( length = a ){
-    var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    var symbols = ["!", "#", "$", "&", "%", "'", "(", ")", "*", "+", "-", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "~", "`", "{", "|", "}", "."];
+function gen_pass() {
 
-    var all = uppercase + lowercase + numbers + symbols;
+    var len = prompt("Choose a password length between 8-128 characters")
+    var uppercase = confirm("Would you like to include uppercase letters in your password?")
+    var lowercase = confirm("Would you like to include lowercase letters in your password?")
+    var numbers = confirm("Would you like to include numbers in your password?")
+    var symbols = confirm("Would you like to include symbols in your password?")
 
-    var password = ""
+    var str = [ ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], ["!", "#", "$", "&", "%", "'", "(", ")", "*", "+", "-", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "~", "`", "{", "|", "}", "."] ];
+    
+    var str_user;
+    uppercase===true ? str_user=str[0] :0;
+    lowercase===true ? str_user=str[1] :0;
+    numbers===true ? str_user=str[2] :0;
+    symbols===true ? str_user=str[3] :0;
 
+    if(str_user){
+        let len_str = str_user.length;
+        var v=Math.floor(Math.random() * str_user.length);
+        var password = str_user[v];
 
-
-
-
-
-    for (var index = 0; index < length; index++) {
-        var character = Math.floor(Math.random() * all.length);
-        password += all.substring(character, character + 1);
+        for (let i = 0 ; i<len ; i++ ) {
+            v=Math.floor(Math.random() * str_user.length);
+            console.log(v);
+            password += str_user[v];
+        }
+        console.log(password);
+        document.getElementById("readonly-form").value = password;
+    }   else {
+        alert("Please choose a password length of BETWEEN 8-128 characters");
     }
-}
 
-return password;
+
+
+}
